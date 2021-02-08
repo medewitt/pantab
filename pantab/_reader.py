29df8cf75784527d8d43a6e7820414a508369b42
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-02-08 13:21:14 -0500

    Update _reader.py

--- pantab/_reader.py
+++ pantab/_reader.py
@@ -40,14 +40,7 @@ def _read_table(*, connection: tab_api.Connection, table: TableType) -> pd.DataF
 
     # TODO: remove this hackery...
     for k, v in dtypes.items():
-        if v == "date":
-            dtypes[k] = "datetime64[ns]"
-    for k, v in dtypes.items():
-        if v == "Int32":
-            dtypes[k] = "object"
-    for k, v in dtypes.items():
-        if v == "int32":
-            dtypes[k] = "object"
+        dtypes[k] = "object"
 
     df = df.astype(dtypes)
     df = df.fillna(value=np.nan)  # Replace any appearances of None
